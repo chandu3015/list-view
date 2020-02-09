866c3759198b2c5f945c01a0b43612a0450f37e3
chandu <chandu3015@gmail.com>
2020-02-09 11:51:41 -0600

    added list module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NumListComponent } from './num-list/num-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NumListComponent
   ],
   imports: [
     BrowserModule
